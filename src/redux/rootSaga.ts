a53aff16f5a663cba114ad601984e7a4195776b7
AbanoubAshraaf <abanoubashrf25@gmail.com>
2022-05-22 20:27:01 +0200

    fix : add sagas

--- src/redux/rootSaga.ts
+++ src/redux/rootSaga.ts
@@ -1,3 +1,9 @@
-function* rootSaga() {}
+import { all, takeLatest } from 'redux-saga/effects';
+import { userLoginSaga, authActionTypes } from './authentication';
+import { getOrderDataSaga, orderActionTypes } from './order';
+function* rootSaga() {
+    yield all([takeLatest(authActionTypes.USER_LOGIN, userLoginSaga)]);
+    yield all([takeLatest(orderActionTypes.GET_ORDER_REQUEST, getOrderDataSaga)]);
+}
 
 export default rootSaga;
