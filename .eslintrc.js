56304419484e1f215cf963c0f5d89bd8602319b6
AbanoubAshraaf <abanoubashrf25@gmail.com>
2022-05-22 16:24:03 +0200

    add eslint and prettierr configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
+    root: true,
+    extends: '@react-native-community',
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    env: {
+        jest: true,
     },
-  ],
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx', '**/*.spec.js', '**/*.spec.jsx'],
+            rules: {
+                '@typescript-eslint/no-shadow': ['error'],
+                'no-shadow': 'off',
+                'no-undef': 'off',
+                'object-shorthand': ['error', 'always'],
+                'react-native/no-inline-styles': 0,
+            },
+        },
+    ],
+    ignorePatterns: ['**/coverage'],
 };
